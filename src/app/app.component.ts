8ebdd3c30fb85f8f76bcdf32b1e871aafb36d954
nazzeya <n.baisakalova01@gmail.com>
2022-12-05 20:50:44 +0600

    just functionality of simple ChatApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IMessage } from './models/message';
+import { messages as data } from './data/messages';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-with-angular';
+  titleOfApp = 'chat app';
+
+  // messages: IMessage[] = [
+  //   {
+  //     user: "User",
+  //     text: "Hello, this is my first message"
+  //   }
+  // ];
+
+  messages: IMessage[] = data
+
+  text: string = "";
+
+  sendMessage() {
+    this.messages.push(
+      {
+        user: "User",
+        text: this.text,
+      })
+
+    var date = new Date();
+
+    if(this.text == 'day') {
+      this.messages.push({
+        user: "Bot",
+        text: String(date.getDate())
+      })
+    } else if (this.text == 'month') {
+      this.messages.push({
+        user: "Bot",
+        text: String(date.getMonth())
+      })
+    } else if (this.text == 'year') {
+      this.messages.push({
+        user: "Bot",
+        text: String(date.getFullYear())
+      })
+    } else {
+      this.messages.push({
+        user: "Bot",
+        text: "I don't have such functionality. Try something else"
+      })
+    }
+
+    console.log(this.messages)
+  }
+
+  inputText: string;
+  clear () {
+    this.inputText = '';
+  }
 }
---
+++ src/app/data/messages.ts
@@ -0,0 +1,8 @@
+import { IMessage } from "../models/message";
+
+export const messages: IMessage[] = [
+    {
+        user: "User 1",
+        text: "Hello, this is my first message. So you can try this bot. Enter 'day', 'month' or 'year'."
+    }
+]
